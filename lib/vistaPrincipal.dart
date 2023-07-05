3d00a028654217e555050128a7189477b2a76a9b
Joyce <josabavi123@gmail.com>
2023-07-04 20:55:27 -0500

    add v14_1

---
+++ lib/vistaPrincipal.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class vistaPrincipal extends StatefulWidget {
+  const vistaPrincipal({super.key});
+
+  @override
+  State<vistaPrincipal> createState() => _vistaPrincipalState();
+}
+
+class _vistaPrincipalState extends State<vistaPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
